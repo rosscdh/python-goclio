f532867472d23515ef2d870e5cad1285c412fdf0
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-08-12 12:30:13 +0200

    progress

--- goclio/clio.py
+++ goclio/clio.py
@@ -1,3 +1,5 @@
+import re
+import json
 import requests
 import urlparse
 
@@ -9,6 +11,7 @@ class BaseApi(object):
     def __init__(self, token, **kwargs):
         self.token = token
         self.response = self.response_json = {}
+        self.params = kwargs
 
     @property
     def auth(self):
@@ -22,28 +25,53 @@ class BaseApi(object):
     def ok(self):
         return getattr(self.response, 'ok', None)
 
+    @property
+    def parse_uri(self):
+        uri = self.uri
+
+        for k, v in self.params.iteritems():
+            key = ':{key}'.format(key=k)
+            uri = uri.replace(key, str(v))
+        
+        return re.sub(r'\/\:(\w)+', '', uri)
+
+    def headers(self, **kwargs):
+        headers = {'Content-Type': 'application/json'}
+        headers.update(kwargs)
+        headers.update(self.auth)
+        return headers
+
+    def wrap_namespace(self, **kwargs):
+        return json.dumps(kwargs)
+
     def endpoint(self, *args, **kwargs):
-        return urlparse.urljoin(self.base_url, self.uri, *args, **kwargs)
+        return urlparse.urljoin(self.base_url, self.parse_uri, *args, **kwargs)
 
     def process(self, response):
-        self.response = response
-        self.response_json = self.response.json()
-        return self.response_json
+        if response.ok is True:
+            self.response = response
+            self.response_json = self.response.json()
+            return self.response_json
+        #
+        # Handle the bad CLI api implementation of 404 returning HTML and not
+        # a valid REST reponse
+        #
+        return {'message': response.reason, 'ok': False, 'status_code': response.status_code, 'url': response.url}
 
     def get(self, **kwargs):
-        return self.process(response=self.r.get(self.endpoint(), headers=self.auth, **kwargs))
+        return self.process(response=self.r.get(self.endpoint(), headers=self.headers(), params=kwargs))
 
     def post(self, **kwargs):
-        return self.process(response=self.r.get(self.endpoint(), headers=self.auth, **kwargs))
+        return self.process(response=self.r.post(self.endpoint(), headers=self.headers(), data=self.wrap_namespace(**kwargs)))
 
     def put(self, **kwargs):
-        return self.process(response=self.r.get(self.endpoint(), headers=self.auth, **kwargs))
+        return self.process(response=self.r.put(self.endpoint(), headers=self.headers(), data=self.wrap_namespace(**kwargs)))
 
     def patch(self, **kwargs):
-        return self.process(response=self.r.get(self.endpoint(), headers=self.auth, **kwargs))
+        return self.process(response=self.r.patch(self.endpoint(), headers=self.headers(), data=self.wrap_namespace(**kwargs)))
 
     def delete(self, **kwargs):
-        return self.process(response=self.r.get(self.endpoint(), headers=self.auth, **kwargs))
+        return self.process(response=self.r.delete(self.endpoint(), headers=self.headers(), params=kwargs))
 
 
 class Me(BaseApi):
@@ -51,18 +79,30 @@ class Me(BaseApi):
 
 
 class Matters(BaseApi):
-    uri = 'matters'
+    uri = 'matters/:id'
+
+    def wrap_namespace(self, **kwargs):
+        return super(Matters, self).wrap_namespace(matter=kwargs)
 
 
 class Documents(BaseApi):
-    uri = 'documents'
+    uri = 'documents/:id'
 
-    class Categories(BaseApi):
-        uri = 'document_categories'
+    def wrap_namespace(self, **kwargs):
+        return {'document': kwargs}
 
-    class Versions(BaseApi):
-        uri = 'documents/:document_id/document_version/:id/download'
+    def document_versions(self):
+        return self.response_json.get('document', {}).get('document_versions', self.get().get('document', {}).get('document_versions',[]))
 
+    def download_version(self, version_id):
+        return self.DownloadVersion(token=self.token, id=self.response_json.get('document', {}).get('id'), document_version=version_id)
+
+    class DownloadVersion(BaseApi):
+        uri = 'documents/:id/document_version/:document_version/download'
+
+
+class DocumentCategories(BaseApi):
+    uri = 'document_categories'
 
 # class Notes(BaseApi):
 #     uri = 'notes'
